00e2ae1e58a1bf0f2a25ba45fb67f3dd5ad0c1a2
Vyacheslav Baumtrok <s.baumtrok2009@yandex.ru>
2024-02-25 12:02:51 +0300

    little fixes

--- src/pages/Purchase/Purchase.tsx
+++ src/pages/Purchase/Purchase.tsx
@@ -2,7 +2,20 @@ import { FC } from "react";
 import styles from './Purchase.module.scss';
 
 const PurchasePage: FC = () => {
-  return <div className={styles.purchase}>Тут прикрутим оплату попозже</div>
-}
+  return (
+  <div className={styles.purchase}>
+    <div className={styles.purchase__grid}>
+      <div>
+        <img src="" alt="basic_plan_img" />
+        <p>Базовый план 1999р/год</p>
+      </div>
+      <div>
+        <img src="" alt="premium_plan_img" />
+        <p>Премиум план 4999р/год</p>
+      </div>
+    </div>
+  </div>
+  )
+};
 
 export default PurchasePage;
